328a806d5d0972637e94c63c96c047cfa233694c
LathifaPuteri <lathifa_puteri_29rpl@student.smktelkom-mlg.sch.id>
2020-11-12 23:28:45 +0700

    Perulangan Ganjil

---
+++ src/studikasusperulanganno/pkg3bilanganganjil/StudiKasusPerulanganNo3BilanganGanjil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studikasusperulanganno.pkg3bilanganganjil;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class StudiKasusPerulanganNo3BilanganGanjil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println("Bilangan Ganjil dari 1 sampai "+n+" adalah:");
+        for (int i=0;i<=n;i++){
+        if (i % 2 == 1){
+            System.out.println(i + "");
+        }
+        
+    }
+    }
+    
+}
